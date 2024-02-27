2f0939ee46a93b7753ef005be01a3e8a9f3cb896
WojtAcht <wachtelik@gmail.com>
2024-02-27 18:52:23 +0100

    feat: add new scale functions

--- distribution_optimization_py/scale.py
+++ distribution_optimization_py/scale.py
@@ -12,6 +12,16 @@ def scale_linearly(
     return target_lower + normalized_x * (target_upper - target_lower)
 
 
+def scale_uniformly_simplex(x: np.ndarray) -> np.ndarray:
+    n = len(x)
+    return np.array([np.power(x_, 1 / (n - k)) for k, x_ in enumerate(x)])
+
+
+def unscale_uniformly_simplex(x: np.ndarray) -> np.ndarray:
+    n = len(x)
+    return np.array([np.power(x_, n - k) for k, x_ in enumerate(x)])
+
+
 def reals_to_full_simplex(
     x: np.ndarray,
 ) -> np.ndarray:
@@ -40,3 +50,17 @@ def reals_to_reals_with_offset(x: np.ndarray) -> np.ndarray:
 
 def reals_with_offset_to_reals(x: np.ndarray) -> np.ndarray:
     return np.concatenate([x[:1], np.diff(x)])
+
+
+def reals_to_uniform_simplex(x: np.ndarray) -> np.ndarray:
+    n = x.size
+    ksi = np.log(x) / np.arange(n, 0, -1)
+    logvalues = np.concatenate((np.log(-np.expm1(ksi)), (0,))) + np.concatenate(((0,), np.cumsum(ksi)))
+    values = np.exp(logvalues - logvalues.max())
+    return values / values.sum()
+
+
+def uniform_simplex_to_reals(x: np.ndarray) -> np.ndarray:
+    n = x.size - 1
+    logvalues = -np.arange(n, 0, -1) * np.log1p(x[:-1] / np.cumsum(x[-1:0:-1])[::-1])
+    return np.exp(logvalues)
