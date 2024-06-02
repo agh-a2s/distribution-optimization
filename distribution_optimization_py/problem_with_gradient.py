227524e9afac860e73fa10351bce5c864a40c80f
WojtAcht <wachtelik@gmail.com>
2024-06-02 13:30:22 +0200

    fix: run pre commit

--- distribution_optimization_py/problem_with_gradient.py
+++ distribution_optimization_py/problem_with_gradient.py
@@ -1,5 +1,6 @@
-import torch
 import numpy as np
+import torch
+
 from .problem import DEFAULT_NR_OF_KERNELS
 from .utils import optimal_no_bins
 
@@ -7,12 +8,7 @@ from .utils import optimal_no_bins
 def cdf_mixtures_torch(
     kernel: torch.Tensor, means: torch.Tensor, sds: torch.Tensor, weights: torch.Tensor
 ) -> torch.Tensor:
-    mixtures = torch.stack(
-        [
-            torch.distributions.Normal(mean_, sd_).cdf(kernel)
-            for mean_, sd_ in zip(means, sds)
-        ]
-    )
+    mixtures = torch.stack([torch.distributions.Normal(mean_, sd_).cdf(kernel) for mean_, sd_ in zip(means, sds)])
     return torch.matmul(mixtures.T, weights)
 
 
@@ -36,15 +32,9 @@ class ChiSquareLossWithGradientModel(torch.nn.Module):
         self.nr_of_modes = nr_of_modes
         self.N = len(data)
         self.nr_of_bins = optimal_no_bins(data)
-        self.breaks = torch.linspace(
-            self.data.min(), self.data.max(), self.nr_of_bins + 1
-        )
-        self.observed_bins = torch.histc(
-            self.data, bins=self.nr_of_bins, min=self.data.min(), max=self.data.max()
-        )
-        self.nr_of_kernels = (
-            nr_of_kernels if nr_of_kernels is not None else DEFAULT_NR_OF_KERNELS
-        )
+        self.breaks = torch.linspace(self.data.min(), self.data.max(), self.nr_of_bins + 1)
+        self.observed_bins = torch.histc(self.data, bins=self.nr_of_bins, min=self.data.min(), max=self.data.max())
+        self.nr_of_kernels = nr_of_kernels if nr_of_kernels is not None else DEFAULT_NR_OF_KERNELS
 
         x = initialize()
         weights, stds, means = (
@@ -58,12 +48,7 @@ class ChiSquareLossWithGradientModel(torch.nn.Module):
 
     def similarity_error(self):
         normalized_weights = self.weights / torch.sum(self.weights)
-        estimated_bins = (
-            bin_prob_for_mixtures_torch(
-                self.means, self.sds, normalized_weights, self.breaks
-            )
-            * self.N
-        )
+        estimated_bins = bin_prob_for_mixtures_torch(self.means, self.sds, normalized_weights, self.breaks) * self.N
         norm = estimated_bins.clone()
         norm[norm < 1] = 1
         diffssq = torch.pow((self.observed_bins - estimated_bins), 2)
